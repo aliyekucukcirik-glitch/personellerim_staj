e197463e7986b3f660543683f8d18f1cefd70f1a
aliye <aliyekucukcirik@gmail.com>
2026-07-20 10:34:37 +0300

    ilk proje kurulumu

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mt-5 text-center">
+      <h1 className="text-primary">Personel Yönetim Sistemi</h1>
+      <p className="lead">Figma tasarımını kodlamaya hazırsın!</p>
+    </div>
+  );
+}
+
+export default App;
